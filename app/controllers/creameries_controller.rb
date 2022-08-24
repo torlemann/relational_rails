8ae09249b9df5a82e9a912729e4554a8d3deff39
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-24 15:57:07 -0700

    Add post to routes and create new view with form

--- app/controllers/creameries_controller.rb
+++ app/controllers/creameries_controller.rb
@@ -2,4 +2,10 @@ class CreameriesController < ApplicationController
     def index
         @creameries = ['Creamery 1', 'Creamery 2', 'Creamery 3']
     end
+
+    def new
+    end
+
+    def create
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/creameries', to: 'creameries#index'
+  get '/creameries/new', to: 'creameries#new'
+  post '/creameries', to: 'creameries#create'
 end
