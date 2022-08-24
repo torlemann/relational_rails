6776d1cbc203afaa58ae97ca97f0e474257a4490
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-24 16:35:30 -0700

    Add create method to creameries controller

--- app/controllers/creameries_controller.rb
+++ app/controllers/creameries_controller.rb
@@ -7,5 +7,19 @@ class CreameriesController < ApplicationController
     end
 
     def create
+        task = Task.create!({
+            name: params[:creamery][:name]
+            date_founded: params[:creamery][:date_founded]
+            owner: params[:creamery][:owner]
+            head_cheesemaker: params[:creamery][:head_cheesemaker]
+            location: params[:creamery][:location]
+            farmstead?: params[:creamery][:farmstead?]
+            acreage: params[:creamery][:acreage]
+            awards_won: params[:creamery][:awards_won]
+        })
+
+        task.save
+
+        redirect_to'/creameries'
     end
 end
