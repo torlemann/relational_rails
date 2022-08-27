4e165e81c01d9ac44b843e15b976096b5277f8bc
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-27 16:25:13 -0700

    Refactor and add user story 5 index view

--- spec/features/creameries/cheeses/index_spec.rb
+++ spec/features/creameries/cheeses/index_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe "creamery's cheeses index" do
   it "displays all cheeses that belong to a creamery" do
     creamery = Creamery.create!(name: "Jasper Hill Farm", date_founded: 2000, owner: "Mateo and Andy Kehler", head_cheesemaker: "Scott Harbour", location: "Vermont", farmstead: true, acreage: 100, awards_won: "All of them")
+    creamery2 = Creamery.create!(name: "Point Reyes", date_founded: 2000, owner: "Bob", head_cheesemaker: "Koob", location: "CA", farmstead: true, acreage: 100, awards_won: "All of them")
     cheese = creamery.cheeses.create!(name: "Winnimere", style: "washed", net_wt: 1.0, milk_type: "cow", veg_rennet: false, raw: true)
     cheese2 = creamery.cheeses.create!(name: "Bayley Hazen Blue", style: "blue-veined", net_wt: 7.0, milk_type: "cow", veg_rennet: false, raw: true)
-    cheese3 = cheeses.create!(name: "Original Blue", style: "blue-veined", net_wt: 7.0, milk_type: "cow", veg_rennet: false, raw: true)
+    cheese3 = creamery2.cheeses.create!(name: "Original Blue", style: "blue-veined", net_wt: 7.0, milk_type: "cow", veg_rennet: false, raw: true)
 
     visit "/creameries/#{creamery.id}/cheeses"
     save_and_open_page
