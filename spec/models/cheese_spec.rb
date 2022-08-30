465da40e9b079641c8a62bf3f849b1c7cc48842f
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-30 13:03:36 -0700

    Add cheese model spec and user story 15 test

---
+++ spec/models/cheese_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Cheese, type: :model do
+    describe 'true records' do
+        it 'only shows vegetarian cheeses' do
+            Creamery.destroy_all
+            creamery = Creamery.create!(name: "Jasper Hill Farm", date_founded: 2000, owner: "Mateo and Andy Kehler", head_cheesemaker: "Scott Harbour", location: "Vermont", farmstead: true, acreage: 100, awards_won: "All of them")
+            cheese = creamery.cheeses.create!(name: "Winnimere", style: "washed", net_wt: 1.0, milk_type: "cow", veg_rennet: false, raw: true)
+            cheese2 = creamery.cheeses.create!(name: "Bayley Hazen Blue", style: "blue-veined", net_wt: 7.0, milk_type: "cow", veg_rennet: false, raw: true)
+
+            expect(Cheese.veg_rennet).to eq([cheese, cheese2])
+        end
+    end
+end
